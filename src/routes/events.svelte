a6772697a83f6f8dd42563559dcfade53c8b285d
vandesm14 <vandesm14@gmail.com>
2021-04-01 20:32:43 -0400

    patch: new event

--- src/routes/events.svelte
+++ src/routes/events.svelte
@@ -10,9 +10,9 @@
   import Header from "../components/Header.svelte";
 	import Article from "../components/Article.svelte";
 	
-	let minutes = Math.round(((new Date('2021-02-01T17:00:00.000Z') - new Date()) / 1000 / 60));
+	let minutes = Math.round(((new Date('2021-04-17T04:00:00.000Z') - new Date()) / 1000 / 60));
 	setInterval(function(){
-		minutes = Math.round(((new Date('2021-02-01T17:00:00.000Z') - new Date()) / 1000 / 60));
+		minutes = Math.round(((new Date('2021-04-17T04:00:00.000Z') - new Date()) / 1000 / 60));
 	}, 10000);
 </script>
 
@@ -25,17 +25,27 @@
 	We host a variety of weekly events such as hackathons, code jams, and even open discussions! All of these events are open to everyone, even late-arrivals! Most of these events last for about a week, and the results come out 1-3 days after.
 </Header>
 <Article title="What's Happening">
-	<h2>Mini-Jam</h2>
-	<strong>Deadline:</strong> February 1st, 12pm EST ({@html Math.round(minutes / 60)} hours and {@html minutes % 60} minutes left)<br>
-	<strong>Submissions:</strong> <a target="_blank" href="https://forms.gle/e7doRjf6oi6dYgCg8">https://forms.gle/e7doRjf6oi6dYgCg8</a>
-	<p>Build either a game or program which the user can only press keys if instructed to! For example, if you have a game in which the user moves around with the WSAD keys, the game will have to strictly tell the user when to press the keys.</p>
-	<p>The one and only requirement:</p>
+	<h2>Sci-Fi Interface Contest</h2>
+	<p><strong><strong>This jam will be based around sci-fi interfaces!</strong></strong> Your UI can be whatever you want, this is a design &amp; creativity contest! The more gizmos, animations, and sci-fi flair, the better! A good reference for your UI would be sci-fi interfaces used in movies and TV shows. Be creative!</p><br>
+	<p>PS: If you only know a language that can&#39;t do GUIs but can do CLIs, that works too! (choose what you&#39;re most comfortable with)</p><br>
+	<p><strong><strong>Winning PRIZE:</strong> 1 month of Discord Nitro</strong></p>
+	<p><strong>Starts on:</strong> Friday, April 2nd
+	<strong>Ends on:</strong> Friday, April 16th</p>
+	<p><strong>Submit To:</strong> &lt;#782047133254942730&gt; or <a href="https://forms.gle/e7doRjf6oi6dYgCg8">https://forms.gle/e7doRjf6oi6dYgCg8</a></p><br>
+	<p><strong><strong>Requirements:</strong></strong></p>
+	<ol>
+	<li>Your UI <strong>MUST</strong> be created with code, no graphic design software or animation software should be used</li>
+	<li>Your UI must have the following:</li>
+	<li>A popup/popout with at least 1 form of input (eg: a right-click menu with buttons)</li>
+	<li>At least 3 different types of input (buttons, sliders, pages, dials, textboxes, dropdowns, etc)</li>
+	</ol>
+	<p><strong><strong>Bonus Points:</strong></strong></p>
 	<ul>
-	<li>The user can only press a key if the program or game tells them to</li>
+	<li>Although functionality isn&#39;t really important outside of a UI standpoint, if you add scripting to your UI to have buttons and knobs change values and change display graphics, you&#39;ll get some sweet bonus points!</li>
+	<li>For the web devs out there, SVG animations would be a great touch!</li>
 	</ul>
-	<p>Everything else is up to you.</p>
-	<i>To further explain: You cannot just say "use wasd to move" and call it a day. Each time the user would need to hit say the "w" key, you'd need to explicitly tell them to every single time</i>
-	<i>Check out the event in our <a target="_blank" href="https://discord.gg/semsxbP">Discord server</a> for more info</i>
+	<p><strong><strong>Judging:</strong></strong><br>
+	This event will be judged by one or more judges (I won&#39;t be a judge as I&#39;ll be participating with you guys 🙌)</p>		
 </Article>
 
 <Footer />
