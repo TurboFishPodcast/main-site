c1ccbe5ab64fe223fd93532ecc2b1487bb2e8b11
Shane Vandegrift <vandesm14@gmail.com>
2021-09-07 18:51:17 -0400

    clean up podcast page

--- src/routes/podcast.svelte
+++ src/routes/podcast.svelte
@@ -9,10 +9,6 @@
 <meta http-equiv="refresh" content="0; url = https://anchor.fm/responsive" />
 
 <Header>The Responsive Podcast</Header>
-<!-- <Article title="Our Podcast">
-	The Responsive Podcast is a podcast that releases eposides at least two times a month with each episode being around 30 minutes to an hour long. We enjoy sharing tips, tricks, and tools for everything development. Leon and I are widely open to feedback, so even simple suggestion such as to turn up one of our mics, please feel free to ask!
-</Article> -->
-
 
 <Article>
 	<i>If this doesn't happen automatically just click the button below<br><br></i>
@@ -20,9 +16,3 @@
 		<Button icon="/social/anchor.svg" href="https://anchor.fm/responsive" external={true}>Redirect</Button>
 	</center>
 </Article>
-
-<!-- <center>
-	<Button href="https://anchor.fm/responsive" icon="/social/anchor.svg" external={true}>
-		Take a Listen
-	</Button>
-</center> -->
